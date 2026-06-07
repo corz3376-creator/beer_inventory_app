93f556d897beeb557d51f096cb2b6bfda82f50f1
corz3376-creator <corz3376@googlemail.com>
2026-06-07 20:35:05 +0300

    Create build.gradle.kts

---
+++ android/android/app/android/build.gradle.kts
@@ -0,0 +1,23 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    afterEvaluate {
+        if (hasProperty("android")) {
+            val androidExtension = extensions.getByType(com.android.build.gradle.BaseExtension::class.java)
+            androidExtension.compileSdkVersion(34)
+        }
+    }
+}
+
+tasks.withType<AbstractArchiveTask>().configureEach {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+}
