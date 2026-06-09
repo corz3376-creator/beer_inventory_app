b00942d7f8880770c4b6f97305833577b3779011
corz3376-creator <corz3376@googlemail.com>
2026-06-09 09:07:15 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-// Using relative paths to fix the "URI doesn't exist" compilation blocks
 import 'providers/inventory_provider.dart';
 import 'screens/home_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => InventoryProvider()),
-      ],
+    ChangeNotifierProvider(
+      create: (context) => InventoryProvider(),
       child: const BeerInventoryApp(),
     ),
   );
