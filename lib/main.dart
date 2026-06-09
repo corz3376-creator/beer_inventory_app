555cb2208638001c73d32977ee7297cc2d113b6a
corz3376-creator <corz3376@googlemail.com>
2026-06-09 08:57:47 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-// Fully-qualified package paths pointing directly to your architecture
-import 'package:beer_inventory_app/providers/inventory_provider.dart';
-import 'package:beer_inventory_app/screens/home_screen.dart';
+// Using relative paths to fix the "URI doesn't exist" compilation blocks
+import 'providers/inventory_provider.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
